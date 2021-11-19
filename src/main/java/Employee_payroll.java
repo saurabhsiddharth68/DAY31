447edb8c34cdf3349c5820ebc24bbcc700feef8e
Saurabh Siddharth <saurabhsiddharth68>
2021-11-19 19:49:07 +0530

    Added: Added Prepared Statement

--- src/main/java/Employee_payroll.java
+++ src/main/java/Employee_payroll.java
@@ -1,25 +1,21 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
-import java.sql.SQLException;
-import java.sql.ResultSet;
+import java.sql.*;
 
 public class Employee_payroll {
 
     public static void main(String[] args) {
         Connection connection = null;
-        Statement stmt = null;
-        String query = "update payroll_services.employee_payroll set salary=300000 where id=2";
+        PreparedStatement stmt = null;
+        String query = "update payroll_services.employee_payroll set salary=300000 where id=3";
         try {
-            connection = null;
             Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver Loaded");
             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306?user = root & password = Siddharth@1309");
             System.out.println("Connection establish");
-            stmt = connection.createStatement();
+            stmt = connection.prepareStatement(query);
             System.out.println("Platform Created");
             stmt.executeUpdate(query);
 
-            System.out.println("Update salary of Nikhil to 300000 successfully");
+            System.out.println("Update salary of Aryan to 300000 successfully");
         } catch (ClassNotFoundException | SQLException ex) {
             ex.printStackTrace();
         } finally {
