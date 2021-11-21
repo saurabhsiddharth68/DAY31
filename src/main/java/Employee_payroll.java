c6eb1fbd6a0f04b9c37dae92863438f422389672
Saurabh Siddharth <saurabhsiddharth68>
2021-11-21 20:43:26 +0530

    Fetch data to The Name

--- src/main/java/Employee_payroll.java
+++ src/main/java/Employee_payroll.java
@@ -1,11 +1,21 @@
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Date;
+import java.util.Scanner;
 
 public class Employee_payroll {
 
     public static void main(String[] args) {
         Connection connection = null;
         PreparedStatement stmt = null;
-        String query = "update payroll_services.employee_payroll set salary=300000 where id=3";
+        String query = "select * form payroll_services.employee_payroll where name = ? ";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Employee Name to fetch Employee Payroll details");
+        String name = sc.next();
+        sc.close();
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("Driver Loaded");
@@ -13,9 +23,17 @@ public class Employee_payroll {
             System.out.println("Connection establish");
             stmt = connection.prepareStatement(query);
             System.out.println("Platform Created");
-            stmt.executeUpdate(query);
-
-            System.out.println("Update salary of Aryan to 300000 successfully");
+            stmt.setString(1, name);
+            ResultSet resultSet = stmt.executeQuery();
+            System.out.println("Data Fetched from DB");
+            if(resultSet.next()){
+                System.out.println("ID ="+ resultSet.getString("id"));
+                System.out.println("name = "+resultSet.getString("name"));
+                System.out.println("Salary =" + resultSet.getString("salary"));
+                System.out.println("Start ="+ resultSet.getString("start"));
+            }else{
+                System.out.println("Data Not Found" + name);
+            }
         } catch (ClassNotFoundException | SQLException ex) {
             ex.printStackTrace();
         } finally {
