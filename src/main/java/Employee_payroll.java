86b5b8bfeccf4242ab14e1b176772f475e2457b3
Saurabh Siddharth <saurabhsiddharth68>
2021-11-21 20:56:07 +0530

    Data Fetch by Start Date

--- src/main/java/Employee_payroll.java
+++ src/main/java/Employee_payroll.java
@@ -1,38 +1,30 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Date;
-import java.util.Scanner;
+import java.sql.SQLSyntaxErrorException;
 
 public class Employee_payroll {
 
     public static void main(String[] args) {
         Connection connection = null;
-        PreparedStatement stmt = null;
-        String query = "select * form payroll_services.employee_payroll where name = ? ";
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Employee Name to fetch Employee Payroll details");
-        String name = sc.next();
-        sc.close();
+        Statement stmt = null;
+        String query = "select * form payroll_services.employee_payroll where start between cast ('2018-01-05' as date) and date(now()) ";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("Driver Loaded");
             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306?user = root & password = Siddharth@1309");
             System.out.println("Connection establish");
-            stmt = connection.prepareStatement(query);
+            stmt = connection.createStatement();
             System.out.println("Platform Created");
-            stmt.setString(1, name);
-            ResultSet resultSet = stmt.executeQuery();
+            ResultSet resultSet = stmt.executeQuery(query);
             System.out.println("Data Fetched from DB");
             if(resultSet.next()){
                 System.out.println("ID ="+ resultSet.getString("id"));
                 System.out.println("name = "+resultSet.getString("name"));
                 System.out.println("Salary =" + resultSet.getString("salary"));
                 System.out.println("Start ="+ resultSet.getString("start"));
-            }else{
-                System.out.println("Data Not Found" + name);
             }
         } catch (ClassNotFoundException | SQLException ex) {
             ex.printStackTrace();
